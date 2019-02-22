30ad103794f0e63d5ab7e49eedfcdb21c7a7e62a
Banurasheena <sheenabanura69@gmail.com>
2019-02-22 16:44:04 +0300

    add login

--- application.py
+++ application.py
@@ -1,33 +1,5 @@
 import os
 
-# from flask import Flask, session
-# from flask_session import Session
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import scoped_session, sessionmaker
-
-# app = Flask(__name__)
-
-# # Check for environment variable
-# if not os.getenv("DATABASE_URL"):
-#     raise RuntimeError("DATABASE_URL is not set")
-
-# # Configure session to use filesystem
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
-
-# # Set up database
-# engine = create_engine(os.getenv("DATABASE_URL"))
-# db = scoped_session(sessionmaker(bind=engine))
-
-
-# @app.route("/")
-# def index():
-#     return "Project 1: TODO
-
-
-
-# my routes
 
 
 
@@ -81,35 +53,8 @@ def register():
         return redirect(url_for('register'), "303")
 
 
-    @app.route('/login', methods=["POST", "GET"])
-    def login():
-        if request.method == "GET":
-            if session.get("logged_in"):
-                flash("You are already logged in")
-                return redirect(url_for('home'), "303")
-            else:
-                return render_template("login.html")
-       
-    
-    if request.method == "POST":
-        username = request.form.get("username")
-        password = request.form.get("password")
+@app.route('/login', methods=["POST", "GET"])
+def login():
+        
+    return render_template(('login.html'))
 
-        res = db.execute("SELECT id, password FROM users WHERE username LIKE :name", {"name": username}).fetchone()
-        db_hash = res.password
-        user_id = res.id
-
-        if not res:
-            flash("User not found")
-            return redirect(url_for('login'), "303")
-
-        # db_hash = db_hash[0].encode("utf-8")
-        if pbkdf2_sha256.verify(password, db_hash):
-            session["logged_in"] = True
-            session["user_id"] = user_id
-            session["user_name"] = username
-            flash("Logged in successful")
-            return redirect(url_for('home'), "303")
-        else:
-            flash("Invalid password")
-            return redirect(url_for('login'), "303")
