5f1da01ac43f7e9def675aa0db6000e521ded963
justinembawomye <jmbawomye@gmail.com>
2019-02-22 14:30:20 +0300

    store hashed passwords

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from forms import RegistrationForm, LoginForm
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -17,6 +18,7 @@ if not os.getenv("DATABASE_URL"):
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SECRET_KEY"]= os.environ.get('SECRET_KEY')
+bcrypt = Bcrypt(app)
 
 Session(app)
 
@@ -37,11 +39,9 @@ def index():
 def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		db.execute("INSERT INTO users(username, email, password) VALUES(:username, :email, :password)",{"username":form.username.data, "email":form.email.data, "password":form.password.data})
+		hashed_password = bcrypt.generate_password_hash(form.password.data)
+		db.execute("INSERT INTO users(username, email, password) VALUES(:username, :email, :password)",{"username":form.username.data, "email":form.email.data, "password":hashed_password})
 		db.commit()
 		flash (f"user {form.username.data} have successfully created an account! Please login", 'success')
 		return redirect(url_for('index'))
 	return render_template('register.html', form=form)
-
-
-
