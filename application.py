1f4271f837071fd7da15fb131936c337cee80db7
justinembawomye <jmbawomye@gmail.com>
2019-02-25 09:26:46 +0300

    create login and home route

--- application.py
+++ application.py
@@ -30,18 +30,36 @@ db = scoped_session(sessionmaker(bind=engine))
 
 
 
-@app.route("/")
-def index():
-    return "Project 1: TODO"
+
+@app.route('/')
+def home():
+	return render_template('home.html')
 
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		hashed_password = bcrypt.generate_password_hash(form.password.data)
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 		db.execute("INSERT INTO users(username, email, password) VALUES(:username, :email, :password)",{"username":form.username.data, "email":form.email.data, "password":hashed_password})
 		db.commit()
 		flash (f"user {form.username.data} have successfully created an account! Please login", 'success')
-		return redirect(url_for('index'))
+		return redirect(url_for('login'))
 	return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = db.execute("SELECT * FROM users WHERE (email=:email)",{'email': form.email.data}).fetchone()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			flash(f"Welcome start searching for your favorite books.", 'success')
+			return redirect(url_for('home'))
+		else:
+			flash("Invalid email or password. Try again", 'danger')			
+	return render_template('login.html', form=form)
+							 
+
+
+
